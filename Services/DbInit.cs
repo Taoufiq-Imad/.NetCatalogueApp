f94ac4248d64d78793946894c59f81ae868f83e1
Taoufiq-Imad <Taoufiq-Imad@users.noreply.github.com>
2021-01-11 16:53:00 +0100

    edit init data

--- Services/DbInit.cs
+++ Services/DbInit.cs
@@ -4,13 +4,13 @@ namespace CatalogueApp.Services
     public static class DbInit
     {
         public static void initData(CatalogueDbRepository CatalogueDb){
-            CatalogueDb.Categories.Add(new Category{Name="Ord"});
+            CatalogueDb.Categories.Add(new Category{Name="ordinateurs"});
             CatalogueDb.Categories.Add(new Category{Name="Imprt"});
             CatalogueDb.Categories.Add(new Category{Name="Table"});
-            CatalogueDb.Products.Add(new Product{Name="hhhh",Price=123,CategoryID=1});
-            CatalogueDb.Products.Add(new Product{Name="yyyyy",Price=323,CategoryID=1});
-            CatalogueDb.Products.Add(new Product{Name="fgh",Price=673,CategoryID=2});
-            CatalogueDb.Products.Add(new Product{Name="hgregeh",Price=453,CategoryID=3});
+            CatalogueDb.Products.Add(new Product{Name="hp",Price=123,CategoryID=1});
+            CatalogueDb.Products.Add(new Product{Name="dell",Price=323,CategoryID=1});
+            CatalogueDb.Products.Add(new Product{Name="hp",Price=673,CategoryID=2});
+            CatalogueDb.Products.Add(new Product{Name="10x20",Price=453,CategoryID=3});
             CatalogueDb.SaveChanges();
         }
     }
